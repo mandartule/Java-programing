0cbf7facd5686a6bcb3bc07e884e86cd1d14f7ea
Mandar Tule <mandartule5@gmail.com>
2023-05-01 15:04:20 +0530

    new file:   Basics/Basic/Hello world.java

---
+++ Basics/Varibles and datatypes/varibles.java
@@ -0,0 +1,14 @@
+public class varibles { 
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 5;
+        String name = "Tony Stark";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
+        
+        a = 50;
+        System.out.println(a);   
+     }
+    
+}
